df808b9066fa690a0a7020b154ad9b1ed426fc58
Gregory Loscombe <greg.loscombe@gmail.com>
2012-03-05 15:01:20 +0100

    Added a hacky script to copy configs round

---
+++ linkconfigs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo $HOME
+echo $PWD
+
+ln -s $PWD/vimrc $HOME/.vimrc
+ln -s $PWD/bashrc $HOME/.bashrc
+ln -s $PWD/gitconfig $HOME/.gitconfig
+ln -s $PWD/vimrc $HOME/.vimrc
+ln -s $PWD/vimrc $HOME/.vimrc
