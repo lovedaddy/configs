e7581698397805a4c5fe9c25b4e944acc8dff140
Gregory Loscombe <greg.loscombe@gmail.com>
2012-03-06 14:22:32 +0100

    Updated what gets copied

--- linkconfigs.sh
+++ linkconfigs.sh
@@ -6,5 +6,5 @@ echo $PWD
 ln -s $PWD/vimrc $HOME/.vimrc
 ln -s $PWD/bashrc $HOME/.bashrc
 ln -s $PWD/gitconfig $HOME/.gitconfig
-ln -s $PWD/vimrc $HOME/.vimrc
-ln -s $PWD/vimrc $HOME/.vimrc
+ln -s $PWD/inputrc $HOME/.inputrc
+ln -s $PWD/dir_colors $HOME/.dir_colors
